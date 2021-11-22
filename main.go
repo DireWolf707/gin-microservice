254bd4ebf15636143be8fe9fd580abc3d5e42166
Rahul Sood <direwolf@pop-os.localdomain>
2021-11-22 18:45:01 +0530

    update recipe endpoint

--- main.go
+++ main.go
@@ -41,10 +41,36 @@ func NewRecipieHandler(c *gin.Context) {
 func ListRecipesHandler(c *gin.Context) {
 	c.JSON(200, recipes)
 }
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+	idx := -1
+	for i, v := range recipes {
+		if v.ID == id {
+			idx = i
+			break
+		}
+	}
+	if idx == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+	recipe.ID = id
+	recipes[idx] = recipe
+	c.JSON(http.StatusOK, recipe)
+}
 
 func main() {
 	router := gin.Default()
 	router.GET("/recipes", ListRecipesHandler)
 	router.POST("/recipes", NewRecipieHandler)
+	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.Run()
 }

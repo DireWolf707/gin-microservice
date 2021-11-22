8963917e154daaa1eb791999472333285e26ec6a
Rahul Sood <direwolf@pop-os.localdomain>
2021-11-22 18:32:56 +0530

    list recipes endpoint

--- main.go
+++ main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
-func main() {
-	router := gin.Default()
-	router.Run()
-}
-
 type Recipe struct {
+	ID           string    `json:"id"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
 	Instructions []string  `json:"instructions"`
 	PublishedAt  time.Time `json:"publishedAt"`
+}
+
+type Recipes []Recipe
+
+var recipes Recipes
+
+func init() {
+	recipes = make(Recipes, 0)
+}
+func NewRecipieHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
+
+func ListRecipesHandler(c *gin.Context) {
+	c.JSON(200, recipes)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/recipes", ListRecipesHandler)
+	router.POST("/recipes", NewRecipieHandler)
+	router.Run()
 }

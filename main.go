e3765768a3e46b400d9282ed5f710d508a66813b
Rahul Sood <direwolf@pop-os.localdomain>
2021-11-23 18:21:56 +0530

    added mongodb integration

--- main.go
+++ main.go
@@ -1,28 +1,46 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/rs/xid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type Recipe struct {
-	ID           string    `json:"id"`
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	Name         string             `json:"name" bson:"name"`
+	Tags         []string           `json:"tags" bson:"tags"`
+	Ingredients  []string           `json:"ingredients" bson:"ingredients"`
+	Instructions []string           `json:"instructions" bson:"instructions"`
+	PublishedAt  time.Time          `json:"publishedAt" bson:"publishedAt"`
 }
 
 type Recipes []Recipe
 
 var recipes Recipes
+var ctx context.Context
+var client *mongo.Client
+var collection *mongo.Collection
+var err error
 
 func init() {
 	recipes = make(Recipes, 0)
+	ctx = context.Background()
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://admin:password@localhost:27017/test?authSource=admin"))
+	if err = client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to mongo")
+	collection = client.Database("demo").Collection("recipes")
 }
 func NewRecipieHandler(c *gin.Context) {
 	var recipe Recipe
@@ -32,15 +50,37 @@ func NewRecipieHandler(c *gin.Context) {
 		})
 		return
 	}
-	recipe.ID = xid.New().String()
+	recipe.ID = primitive.NewObjectID()
 	recipe.PublishedAt = time.Now()
-	recipes = append(recipes, recipe)
+	_, err = collection.InsertOne(ctx, recipe)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Error while inserting a new recipe",
+		})
+		return
+	}
 	c.JSON(http.StatusOK, recipe)
 }
 
 func ListRecipesHandler(c *gin.Context) {
-	c.JSON(200, recipes)
+	cur, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	defer cur.Close(ctx)
+	recipes := make(Recipes, 0)
+	for cur.Next(ctx) {
+		var recipe Recipe
+		cur.Decode(&recipe)
+		recipes = append(recipes, recipe)
+	}
+	c.JSON(http.StatusOK, recipes)
 }
+
 func UpdateRecipeHandler(c *gin.Context) {
 	id := c.Params.ByName("id")
 	var recipe Recipe
@@ -49,33 +89,35 @@ func UpdateRecipeHandler(c *gin.Context) {
 			"error": err.Error(),
 		})
 	}
-	idx := -1
-	for i, v := range recipes {
-		if v.ID == id {
-			idx = i
-			break
-		}
-	}
-	if idx == -1 {
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "Recipe not found",
-		})
+	primitive.ObjectIDFromHex(id)
+	objectId, _ := primitive.ObjectIDFromHex(id)
+
+	_, err = collection.UpdateOne(ctx, bson.M{
+		"_id": objectId,
+	}, bson.D{{"$set", bson.D{
+		{"name", recipe.Name},
+		{"instructions", recipe.Instructions},
+		{"ingredients", recipe.Ingredients},
+		{"tags", recipe.Tags},
+	}}})
+	if err != nil {
+
+		fmt.Println(err)
+
+		c.JSON(http.StatusInternalServerError,
+
+			gin.H{"error": err.Error()})
+
 		return
+
 	}
-	recipe.ID = id
-	recipes[idx] = recipe
 	c.JSON(http.StatusOK, recipe)
 }
 
 func DeleteRecipeHandler(c *gin.Context) {
-	id := c.Params.ByName("id")
+	//id := c.Params.ByName("id")
 	idx := -1
-	for i, v := range recipes {
-		if v.ID == id {
-			idx = i
-			break
-		}
-	}
+
 	if idx == -1 {
 		c.JSON(http.StatusNotFound, gin.H{
 			"error": "Recipe not found",
@@ -88,11 +130,16 @@ func DeleteRecipeHandler(c *gin.Context) {
 	})
 }
 
+func SearchRecipesHandler(c *gin.Context) {
+	//tag:=c.Query("tag")
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/recipes", ListRecipesHandler)
 	router.POST("/recipes", NewRecipieHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.DELETE("/recipes/:id", DeleteRecipeHandler)
+	router.GET("/recipes/search", SearchRecipesHandler)
 	router.Run()
 }
